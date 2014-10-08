321d121eb746bac79a805a185d4f58f2eb582f1e
Thomas Jeon <thomasjeon@thomass-mbp.lrei>
2014-10-08 15:09:19 -0400

    test bottom sticky

--- scrollLock.js
+++ scrollLock.js
@@ -1,14 +1,34 @@
-$.fn.sidebarScrollLock = function (pos) {
-  var $this = this,
-      $window = $(window);
-      $sidebar $('#sidebar');
+$(document).ready(function () {
+  $.fn.scrollLock = function () {
+    var $this = this,
+        $window = $(window);
 
-  var $sidebarLength = $sidebar.height();
-      $sidebarTop = $sidebar.offset();
+    var $thisHeight = $this.height();
+        $thisTop = $this.offset().top;
 
-  $window.scroll(function (e) {
-  
-    //while scroll down if you pass side bar legth, sticky side bar bottom
-    //while scroll up if you sticky side bar top if you havent reach the top of side bar yet 
-  });
-};
+    var $lastScrollTop = $window.scrollTop();
+    $window.scroll(function (e) {
+
+      var $windowHeight = $window.height();
+          $currentScrollTop = $window.scrollTop();
+
+      if( $currentScrollTop > $lastScrollTop ){
+        //scroll down
+        if( $currentScrollTop + $windowHeight == $thisHeight + $thisTop ){
+          //sticky the bottom 
+          $this.css({
+            position: 'fixed',
+            bottom: 0
+          });
+        }
+        //if past the side bar container, then unsticky
+        //while scroll down if you pass side bar legth, sticky side bar bottom
+      }else{
+        //scroll up
+        //while scroll up if you sticky side bar top if you havent reach the top of side bar yet 
+      }
+    });
+  };
+});
+
+$('#sidebar').scrollLock();
