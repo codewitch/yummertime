1f723ce5458deaee985ee0c01d2d359753458c12
Thomas Jeon <thomasjeon@thomass-mbp.lrei>
2014-10-08 17:37:04 -0400

    scroll locks both ways

--- scrollLock.js
+++ scrollLock.js
@@ -3,32 +3,93 @@ $(document).ready(function () {
     var $this = this,
         $window = $(window);
 
-    var $thisHeight = $this.height();
-        $thisTop = $this.offset().top;
-
     var $lastScrollTop = $window.scrollTop();
+        $currentStatus = 0;
+        // 0 = scrolling, 1 = sticky top, 2 = sticky bottom
+
+    var $thisTop = $this.offset().top;
+        $headerOffset = 60;
     $window.scroll(function (e) {
 
-      var $windowHeight = $window.height();
+      var $thisHeight = $this.outerHeight(false);
+          $thisCurrentTop = $this.offset().top;
+          $parentHeight = $this.parent().outerHeight(false);
+          $parentTop = $this.parent().offset().top;
+          $windowHeight = $window.height();
           $currentScrollTop = $window.scrollTop();
+
+      var $windowBottom = $currentScrollTop + $windowHeight;
 
       if( $currentScrollTop > $lastScrollTop ){
         //scroll down
-        if( $currentScrollTop + $windowHeight == $thisHeight + $thisTop ){
-          //sticky the bottom 
-          $this.css({
-            position: 'fixed',
-            bottom: 0
-          });
+        if( $windowBottom >= $thisHeight + $thisCurrentTop ){
+          //if at end of side bar and not yet at end of main section, sticky the bottom
+          if( $windowBottom < $parentHeight + $parentTop ){
+            $this.css({
+              position: 'fixed',
+              bottom: 0,
+              top: ''
+            });
+            $currentStatus = 2;
+          }else{
+            //else, aboslute bottom position
+            $this.css({
+              position: 'absolute',
+              bottom: 0,
+              top: ''
+            });
+            $currentStatus = 0;
+          }
+        }else{
+          //window bottom is in the middle of sidebar
+          if( $currentStatus == 1 ){
+            //if scroll down while side bar is sticky top
+            //absolute position it.
+            $this.css({
+              position: 'absolute',
+              top: $currentScrollTop - $parentTop + $headerOffset, 
+              bottom: ''
+            });
+            $currentStatus = 0;
+          }
         }
-        //if past the side bar container, then unsticky
-        //while scroll down if you pass side bar legth, sticky side bar bottom
       }else{
         //scroll up
-        //while scroll up if you sticky side bar top if you havent reach the top of side bar yet 
+        if( $currentScrollTop <= $thisCurrentTop - $headerOffset ){
+          //if at top of side bar and not yet top of main section, sticky the top
+          if( $currentScrollTop > $parentTop ){
+            $this.css({
+              position: 'fixed',
+              top: $headerOffset,
+              bottom: ''
+            });
+            $currentStatus = 1;
+          }else{
+            $this.css({
+              position: 'relative',
+              top: '',
+              bottom: ''
+            });
+            $currentStatus = 0;
+          }
+        }else{
+          //window top is in the middle of sidebar
+          if( $currentStatus == 2 ){
+            //if scroll up while side bar is sticky bottom
+            //absolute position it.
+            $this.css({
+              position: 'absolute',
+              bottom: $parentTop + $parentHeight - $currentScrollTop - $windowHeight, 
+              top: ''
+            });
+            $currentStatus = 0;
+          }
+        }
       }
+      $lastScrollTop = $currentScrollTop;
     });
   };
+
+  $('#sidebar').scrollLock();
 });
 
-$('#sidebar').scrollLock();
